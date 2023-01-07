b0dbe0744a82aca377bf73c7ae3d0d7c6162f9ce
MobinurRahaman <mobinurrahaman@gmail.com>
2023-01-07 17:46:09 +0530

    Fixed not getting  channel data from database

--- src/LiveTv.js
+++ src/LiveTv.js
@@ -14,13 +14,28 @@ db.version(1).stores({
 export default function LiveTv() {
   const { channelId } = useParams();
   const [currentChannelData, setCurrentChannelData] = useState([]);
+  const [error, setError] = useState({ code: null, message: "" });
 
   useLiveQuery(() => {
     db.open().then(() => {
       db.playlists.toArray().then((result) => {
-        setCurrentChannelData(
-          result[0].data.filter((item) => item.tvg.id === channelId)[0]
+        const channelDataMatchesWithEmptyArrays = result.map((playlistItem) => {
+          const channelDataMatchesWithEmptyArrays = playlistItem.data.find(
+            (channelItem) => channelItem.tvg.id === channelId
+          );
+          return channelDataMatchesWithEmptyArrays
+            ? [channelDataMatchesWithEmptyArrays]
+            : [];
+        });
+        const channelDataMatches = channelDataMatchesWithEmptyArrays.find(
+          (array) => array.length > 0
         );
+        channelDataMatches
+          ? setCurrentChannelData(channelDataMatches[0])
+          : setError({
+              code: 404,
+              message: "No channel found for this channel id",
+            });
       });
     });
   }, [channelId]);
@@ -34,16 +49,20 @@ export default function LiveTv() {
 
   return (
     <>
-      <Player>
-        <Hls version="latest" config={hlsConfig} poster="">
-          <source
-            data-src={currentChannelData?.url}
-            type="application/x-mpegURL"
-          />
-        </Hls>
-        <DefaultUi />
-        {/* ... */}
-      </Player>
+      {error.message ? (
+        <>{error.message}</>
+      ) : (
+        <Player>
+          <Hls version="latest" config={hlsConfig} poster="">
+            <source
+              data-src={currentChannelData?.url}
+              type="application/x-mpegURL"
+            />
+          </Hls>
+          <DefaultUi />
+          {/* ... */}
+        </Player>
+      )}
     </>
   );
 }
